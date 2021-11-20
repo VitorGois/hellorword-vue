5ddf2f26c788abf47850cf4280c5dd08b67d609f
Vitor Gois <vitorcgois777@gmail.com>
2021-11-20 01:16:35 -0300

    change to separated component file

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,12 @@
 <template>
   <v-app>
-    <v-system-bar
-      height="15"
-    >
-      <v-spacer></v-spacer>
-      <v-icon>mdi-wifi-strength-4</v-icon>
-      <v-icon>mdi-signal-cellular-outline</v-icon>
-      <v-icon>mdi-battery</v-icon>
-      <span>12:30</span>
-    </v-system-bar>
-
-    <v-app-bar
-      color="green accent-2"
-      dense
-      dark
-    >
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
-
-      <v-toolbar-title>VueAnimals</v-toolbar-title>
-
-      <v-spacer></v-spacer>
-
-      <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
-
-      <!-- <v-menu
-        left
-        bottom
-      >
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn
-            icon
-            v-bind="attrs"
-            v-on="on"
-          >
-            <v-icon>mdi-dots-vertical</v-icon>
-          </v-btn>
-        </template>
-
-        <v-list>
-          <v-list-item
-            v-for="n in 5"
-            :key="n"
-            @click="() => {}"
-          >
-            <v-list-item-title>Option {{ n }}</v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-menu> -->
-    </v-app-bar>
+    <Navbar />
 
     <v-main>
       <router-view/>
     </v-main>
     
-    <v-footer 
+    <!-- <v-footer 
       padless 
       color="green accent-1"
     >
@@ -89,16 +40,20 @@
 
         <v-icon>mdi-map-marker</v-icon>
       </v-btn>
-    </v-bottom-navigation>
+    </v-bottom-navigation> -->
   </v-app>
 </template>
 
 <script>
+  import Navbar from './components/Navbar'
+  
   export default {
+    components: { Navbar },
     name: 'App',
-
-    data: () => ({
-      //
-    }),
-  };
+    data () {
+      return {
+        
+      }
+    }
+  }
 </script>
