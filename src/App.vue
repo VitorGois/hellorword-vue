e2c3147fdfe67a452ae8b3fe15e7df8f54462701
Vitor Gois <vitorcgois777@gmail.com>
2021-11-20 00:18:49 -0300

    add bottom navigation

--- src/App.vue
+++ src/App.vue
@@ -54,7 +54,6 @@
     <v-main>
       <router-view/>
     </v-main>
-
     
     <v-footer 
       padless 
@@ -67,6 +66,30 @@
         {{ new Date().getFullYear() }} — <strong>Vitor Gois 190810</strong>
       </v-col>
     </v-footer>
+
+    <v-bottom-navigation
+      v-model="value"
+      :input-value="active"
+      color="indigo"
+    >
+      <v-btn>
+        <span>Recents</span>
+
+        <v-icon>mdi-history</v-icon>
+      </v-btn>
+
+      <v-btn>
+        <span>Favorites</span>
+
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn>
+        <span>Nearby</span>
+
+        <v-icon>mdi-map-marker</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
   </v-app>
 </template>
 
