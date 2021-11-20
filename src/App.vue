54c19eb80a14addd97ea187ac2b7254caf3379f5
Vitor Gois <vitorcgois777@gmail.com>
2021-11-20 00:18:07 -0300

    add app bar

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,55 @@
 <template>
   <v-app>
-    <navbar />
+    <v-system-bar
+      height="15"
+    >
+      <v-spacer></v-spacer>
+      <v-icon>mdi-wifi-strength-4</v-icon>
+      <v-icon>mdi-signal-cellular-outline</v-icon>
+      <v-icon>mdi-battery</v-icon>
+      <span>12:30</span>
+    </v-system-bar>
+
+    <v-app-bar
+      color="green accent-2"
+      dense
+      dark
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>VueAnimals</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <!-- <v-menu
+        left
+        bottom
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            icon
+            v-bind="attrs"
+            v-on="on"
+          >
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item
+            v-for="n in 5"
+            :key="n"
+            @click="() => {}"
+          >
+            <v-list-item-title>Option {{ n }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu> -->
+    </v-app-bar>
 
     <v-main>
       <router-view/>
@@ -9,14 +58,9 @@
 </template>
 
 <script>
-  import Navbar from './components/Navbar'
-
   export default {
     name: 'App',
 
-    components: {
-      Navbar,
-    },
     data: () => ({
       //
     }),
