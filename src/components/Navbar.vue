1ac9dfb66183244930f728b5ae9be5a9db387b63
Vitor Gois <vitorcgois777@gmail.com>
2021-11-19 23:55:02 -0300

    apply v-system-bar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <v-system-bar
+      height="15"
+      color="blue-grey lighten-1"
+    >
+      <v-spacer></v-spacer>
+      <v-icon>mdi-wifi-strength-4</v-icon>
+      <v-icon>mdi-signal-cellular-outline</v-icon>
+      <v-icon>mdi-battery</v-icon>
+      <span>12:30</span>
+    </v-system-bar>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Navbar'
+  }
+</script>
