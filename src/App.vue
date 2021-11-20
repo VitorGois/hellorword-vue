a940d6efbccfd1a1d4ab4ef6f710709b995c805f
Vitor Gois <vitorcgois777@gmail.com>
2021-11-20 01:46:13 -0300

    add footer components

--- src/App.vue
+++ src/App.vue
@@ -6,49 +6,17 @@
       <router-view/>
     </v-main>
     
-    <!-- <v-footer 
-      padless 
-      color="green accent-1"
-    >
-      <v-col
-        class="text-center"
-        cols="12"
-      >
-        {{ new Date().getFullYear() }} — <strong>Vitor Gois 190810</strong>
-      </v-col>
-    </v-footer>
+    <Footerbar />
 
-    <v-bottom-navigation
-      v-model="value"
-      :input-value="active"
-      color="indigo"
-    >
-      <v-btn>
-        <span>Recents</span>
-
-        <v-icon>mdi-history</v-icon>
-      </v-btn>
-
-      <v-btn>
-        <span>Favorites</span>
-
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
-
-      <v-btn>
-        <span>Nearby</span>
-
-        <v-icon>mdi-map-marker</v-icon>
-      </v-btn>
-    </v-bottom-navigation> -->
   </v-app>
 </template>
 
 <script>
   import Navbar from './components/Navbar'
+  import Footerbar from './components/Footerbar'
   
   export default {
-    components: { Navbar },
+    components: { Navbar, Footerbar },
     name: 'App',
     data () {
       return {
---
+++ src/components/Footerbar.vue
@@ -0,0 +1,45 @@
+<template>
+  <nav>
+    <v-footer 
+      padless 
+      color="green accent-1"
+    >
+      <v-col
+        class="text-center"
+        cols="12"
+      >
+        {{ new Date().getFullYear() }} — <strong>Vitor Gois 190810</strong>
+      </v-col>
+    </v-footer>
+
+    <v-bottom-navigation
+      v-model="value"
+      :input-value="active"
+      color="indigo"
+    >
+      <v-btn>
+        <span>Recents</span>
+
+        <v-icon>mdi-history</v-icon>
+      </v-btn>
+
+      <v-btn>
+        <span>Favorites</span>
+
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn>
+        <span>Nearby</span>
+
+        <v-icon>mdi-map-marker</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
+  </nav>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
